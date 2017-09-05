0824260fb44be6806755f2e3a8d5b97d38e34cd8
Montagna <javier.a.montagna@accenture.com>
2017-09-05 09:10:01 -0300

    Added WEBConfig on root level to fix 404 issue

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,24 @@ var del = require("del");
 var sourcemaps = require('gulp-sourcemaps');
 
 /*
- * Cpy node server to dist folder.
+ * Copy node server to dist folder.
  */
 gulp.task("server", function () {
     return gulp.src(["index.js", "package.json"], { cwd: "server/**" })
         .pipe(gulp.dest("dist"));
 });
 
+/*
+ * Copy webconfig file to dist folder.
+ */
+gulp.task("webconfig", function () {
+    return gulp.src(["web.config"], { cwd: "./" })
+        .pipe(gulp.dest("dist"));
+});
 
 /**
  * Build the project.
  */
-gulp.task("default", ['server'], function () {
+gulp.task("default", ['server','webconfig'], function () {
     console.log("Building the project ...");
 });
