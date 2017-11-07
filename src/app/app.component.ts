548a2b634321452554d6375dca6f0412937bbe20
Montagna <javier.a.montagna@accenture.com>
2017-11-07 18:52:15 -0300

    Added welcome message and drop down for users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { IUser } from 'app/users/user';
 import { LoginStatusValidatorService } from 'app/shared/login-status-validator.service';
 import { UserService } from 'app/users/user.service';
 import { logInViewModel } from 'app/users/logInViewModel';
+import { MaterializeAction } from 'angular2-materialize';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +20,7 @@ export class AppComponent implements OnInit {
   isUserLogged: boolean = false;
   errorMessage: string = "";
   emailRegex: string = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
+  modalActions = new EventEmitter<string | MaterializeAction>();
 
   ngOnInit(): void {
     if (localStorage.getItem("currentUser") != null) {
@@ -44,12 +46,13 @@ export class AppComponent implements OnInit {
       var logInModel: logInViewModel = new logInViewModel();
       logInModel.email = this.loginForm.controls['email'].value;
       logInModel.password = this.loginForm.controls['password'].value;
-      
+
       this._userService.GetUserByEmailAndPassword(logInModel)
         .subscribe(user => {
           this.user = user;
           this.loginStatusValidatorService.updateLogStatus(true);
           this.errorMessage = "";
+          this.closeModal();
         },
         error => {
           this.errorMessage = <any>error;
@@ -59,5 +62,14 @@ export class AppComponent implements OnInit {
     } else {
       this.formSubmitted = true;
     }
+  }
+
+  closeModal() {
+    this.modalActions.emit({ action: "modal", params: ['close'] });
+  }
+
+  Salir() {
+    localStorage.setItem("currentUser", "");
+    this.isUserLogged = false;
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isUserLogged: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.isUserLogged = localStorage.getItem("currentUser") != "";
   }
 
 }
